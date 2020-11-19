6b1cc9036c4f68dc30322cc4173b1cdaabc10006
Adam <afneve@gmail.com>
2020-11-18 21:09:54 -0600

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function(e) {
+        console.log('Scrolling');
+    });
+  });
+
+// $(window).on("load", function(){
+   
+//   });
